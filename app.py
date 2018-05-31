d6c89f21bc5206aeb78c0bbbee04d3aaee287919
Clement Leung <clhleung@ucsc.edu>
2018-05-31 10:11:47 -0700

    adding in some fixed code

---
+++ app.py
@@ -0,0 +1,42 @@
+import datetime
+import os
+
+from flask import Flask, render_template, redirect, url_for
+from forms import ItemForm
+from models import Items
+from database import db_session, init_db
+
+app = Flask(__name__)
+app.secret_key = os.environ['APP_SECRET_KEY']
+
+@app.before_first_request
+def init_db_connection():
+    init_db()
+    
+@app.teardown_appcontext
+def shutdown_session(exception=None):
+    db_session.remove()
+
+@app.route("/", methods=('GET', 'POST'))
+def add_item():
+    form = ItemForm()
+    if form.validate_on_submit():
+        item = Items(name=form.name.data, quantity=form.quantity.data, description=form.description.data, date_added=datetime.datetime.now())
+        db_session.add(item)
+        db_session.commit()
+        return redirect(url_for('success'))
+    return render_template('index.html', form=form)
+
+@app.route("/success")
+def success():
+    results = []
+ 
+    qry = db_session.query(Items)
+    results = qry.all()
+
+    return str(results)
+  
+
+if __name__ == '__main__':
+    app.debug = True
+    app.run(host='0.0.0.0', port=5001)
